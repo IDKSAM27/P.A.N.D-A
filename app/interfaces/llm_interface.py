223466f09bd97d78ffd7c1e95217dbf4c5e68864
Sampreet <sampreetpatil270@gmail.com>
2025-07-20 12:26:20 +0530

    llm_interface intention and base code added.

--- app/interfaces/llm_interface.py
+++ app/interfaces/llm_interface.py
@@ -1,6 +1,27 @@
-from typing import Dict
+from abc import ABC, abstractmethod
+from typing import List
+from app.models.intent import Intent
 
-class LLMParserInterface():
+class LLMInterface(ABC):
+    """
+    Abstract Base Class for a Large Language Model parser. 
+
+    This interface defines the contrast for any class that aims to parse
+    a natural language command into a structured Intent object.
+    """
+
     @abstractmethod
-    def parse_intent(self, command: str) -> Dict:
+    def parse_command(self, command: str, df_columns: List[str]) -> Intent:
+        """
+        Parses a natural language command to extract a structured intent.
+
+        Args:
+            command(str): The natural language command from the user.
+            df_columns (List[str]): A list of column names from the dataframe
+            to provide context to the LLM.
+
+        Returns:
+            Intent: A structured Intent object representing the user's request.
+        """
+
         pass
